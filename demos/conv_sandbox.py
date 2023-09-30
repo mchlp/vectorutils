0f5541f4d887d7f4af3a987cfe0302a6bf4513e4
Michael Pu <michael.pu@uwaterloo.ca>
2023-09-30 00:30:11 -0400

    Add conversion sandbox script

--- demos/conv_sandbox.py
+++ demos/conv_sandbox.py
@@ -2,10 +2,11 @@ import time
 import faiss
 import sys
 
+from pympler import asizeof
 from vectorutils import conv
 from tqdm import tqdm
-
-from vectorutils.demos import util
+import pyarrow.parquet as pq
+import pyarrow as pa
 
 
 INDEX_PATH = "/u9/yzpu/.cache/pyserini/indexes/faiss.msmarco-v1-passage.tct_colbert-v2-hnp.20210608.5f341b.53bcaa78ab0ca629f3379b8aa00eb3ae/index"
@@ -13,7 +14,7 @@ EPSILON = 1e-9
 
 index = faiss.read_index(INDEX_PATH)
 num_vectors = index.ntotal
-print(f"Size of index: {util.total_size(index)}")
+print(f"Size of index: {asizeof.asizeof(index)}")
 print()
 
 print("Convert to arrow")
@@ -22,7 +23,13 @@ converter = conv.FaissConverter(index)
 arrow_vector = converter.to_arrow()
 end = time.time()
 print(f"Conversion took {end-start}s")
-print(f"Size of vector: {util.total_size(arrow_vector)}")
+print(f"Size of vector: {asizeof.asizeof(arrow_vector)}")
+
+arrow_vector_sub = pa.RecordBatch.from_arrays([arrow_vector.slice(length=500000)], names=["data"])
+
+with pa.OSFile('msmacro_vectors.arrow', 'wb') as sink:
+    with pa.ipc.new_file(sink, schema=arrow_vector_sub.schema) as writer:
+        writer.write(arrow_vector_sub)
 
 print()
 print("Convert to numpy")
@@ -31,7 +38,7 @@ converter = conv.FaissConverter(index)
 numpy_vector = converter.to_numpy()
 end = time.time()
 print(f"Conversion took {end-start}s")
-print(f"Size of vector: {util.total_size(numpy_vector)}")
+print(f"Size of vector: {asizeof.asizeof(numpy_vector)}")
 print(sys.getsizeof(numpy_vector))
 print(numpy_vector.nbytes)
 print(numpy_vector.size)
